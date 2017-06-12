0a7b4c8579057a206662f3c862634f8e68e5eb92
ayesha <ayesh76@msn.com>
2017-06-12 19:49:35 +0200

    Added validation in the model class

--- PinkGarage/Models/ParkedVehicle.cs
+++ PinkGarage/Models/ParkedVehicle.cs
@@ -11,11 +11,25 @@ namespace PinkGarage.Models {
         public int ID { get; set; }
         [Required]
         public string RegNum { get; set; }
+
         public Type Type { get; set; }
+
+        [Required]
+        [StringLength (1024)]
         public string Color { get; set; }
+
+        [Required]
+        [StringLength(1024)]
         public string Brand { get; set; }
+
+        [Required]
+        [StringLength(1024)]
         public string Model { get; set; }
+
+        [Range(1, 10)]
+        [Required]
         public int NumOfWheels { get; set; }
+
         public DateTime CheckInTime { get; set; }
 
     }
