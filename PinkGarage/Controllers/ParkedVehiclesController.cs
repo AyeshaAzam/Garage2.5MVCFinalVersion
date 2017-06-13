f5d2894e3926d9f5ad1a67d120f477fed15b1fce
User <User@76Z7J32>
2017-06-13 16:20:52 +0200

    init sort n filt

--- PinkGarage/Controllers/ParkedVehiclesController.cs
+++ PinkGarage/Controllers/ParkedVehiclesController.cs
@@ -14,11 +14,91 @@ namespace PinkGarage.Controllers
     {
         private GarageDbContext db = new GarageDbContext();
 
-        // GET: ParkedVehicles
-        public ActionResult Index()
+        //// GET: ParkedVehicles
+        //public ActionResult Index()
+        //{
+        //    return View(db.ParkedVehicles.ToList());
+        //}
+
+
+
+        
+
+        // GET: ParkedVehicles // adding Sorting Functionality
+        public ActionResult Index(string orderBy, string searchString)
         {
-            return View(db.ParkedVehicles.ToList());
+            ViewBag.Error = "";
+            ViewBag.SortByRegNum = orderBy == "RegNum" ? "RegNum_desc" : "RegNum";
+            ViewBag.SortByType = orderBy == "Type" ? "Type_desc" : "Type";
+            ViewBag.SortByColor  = orderBy == "Color" ? "Color_desc" : "Color";
+            ViewBag.SortByBrand = orderBy == "Brand" ? "Brand_desc" : "Brand";
+            ViewBag.SortByCheckIn = orderBy == "CheckInTime" ? "CheckInTime_desc" : "CheckInTime";
+
+            var vehicles = db.ParkedVehicles.Select(p => p);
+           
+
+            //search by RegNum 
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                vehicles = vehicles.Where(p => p.RegNum.Equals(searchString));
+                if (vehicles.Count() == 0)
+                    ViewBag.Error = "RegNumber is Invalid, Please try again!";
+                    //ModelState.AddModelError("", "");
+
+            }
+
+
+            switch (orderBy)
+            {
+                case "RegNum":
+                    vehicles = vehicles.OrderBy(p => p.RegNum);
+                    break;
+                // the default sort order is ascending
+                case "RegNum_desc":
+                    vehicles = vehicles.OrderByDescending(p => p.RegNum);
+                    break;
+                case "Type":
+                    vehicles = vehicles.OrderBy(p => p.Type);
+                    break;
+                case "Type_desc":
+                    vehicles = vehicles.OrderByDescending(p => p.Type);
+                    break;
+
+                case "Color":
+                    vehicles = vehicles.OrderBy(p => p.Color);
+                    break;
+                case "Color_desc":
+                    vehicles = vehicles.OrderByDescending(p => p.Color);
+                    break;
+
+                case "Brand":
+                    vehicles = vehicles.OrderBy(p => p.Brand);
+                    break;
+                case "Brand_desc":
+                    vehicles = vehicles.OrderByDescending(p => p.Brand);
+                    break;
+                case "CheckInTime":
+                    vehicles = vehicles.OrderBy(p => p.CheckInTime);
+                    break;
+                case "CheckInTime_desc":
+                    vehicles = vehicles.OrderByDescending(p => p.CheckInTime);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+            return View(vehicles.ToList());
         }
+
+
+
+
+
+
+
 
         // GET: ParkedVehicles/Details/5
         public ActionResult Details(int? id)
@@ -49,7 +129,7 @@ namespace PinkGarage.Controllers
         public ActionResult Create([Bind(Include = "ID,RegNum,Type,EngineType, Color,Brand,Model,NumOfWheels")] ParkedVehicle parkedVehicle)
         {
             if (ModelState.IsValid)
-            {
+            {   
                 parkedVehicle.CheckInTime = DateTime.Now;
                 db.ParkedVehicles.Add(parkedVehicle);
                 db.SaveChanges();
